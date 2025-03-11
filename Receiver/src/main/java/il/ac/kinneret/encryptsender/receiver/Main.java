447e6beddb18cd864bf373b897d36dcb4d19b077
Michael J. May <mjmay-kinneret@users.noreply.github.com>
2025-03-11 11:22:14 +0200

    Added base project files

---
+++ Receiver/src/main/java/il/ac/kinneret/encryptsender/receiver/Main.java
@@ -0,0 +1,12 @@
+package il.ac.kinneret.encryptsender.receiver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello world!");
+    }
+
+    private static void showUsage() {
+        System.out.printf("Hello world!");
+    }
+}
