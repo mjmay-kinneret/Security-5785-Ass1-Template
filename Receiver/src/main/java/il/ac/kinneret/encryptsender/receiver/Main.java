ff944e2be2610cf314133888ee7c19c6cfe12bce
Michael J. May <mjmay-kinneret@users.noreply.github.com>
2025-03-11 13:43:12 +0200

    minor change to hello world template print

--- Receiver/src/main/java/il/ac/kinneret/encryptsender/receiver/Main.java
+++ Receiver/src/main/java/il/ac/kinneret/encryptsender/receiver/Main.java
@@ -3,10 +3,10 @@ package il.ac.kinneret.encryptsender.receiver;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.println("Hello world!");
     }
 
     private static void showUsage() {
-        System.out.print("Hello world!");
+        System.out.println("Hello world!");
     }
 }
--- Sender/src/main/java/il/ac/kinneret/encryptsender/sender/Main.java
+++ Sender/src/main/java/il/ac/kinneret/encryptsender/sender/Main.java
@@ -3,7 +3,7 @@ package il.ac.kinneret.encryptsender.sender;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.println("Hello world!");
     }
 
     private static void showUsage() {
